838c8b79e56654e162efcf92e78e06f5915dbac6
AKN\sixin.gao <sixin.gao@accunome.com.cn>
2024-02-18 17:22:00 +0800

    Add project files.

---
+++ BookRepoTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BookRepoTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
